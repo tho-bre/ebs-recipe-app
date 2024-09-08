88d4565d178626353f93a03554536d59462bb642
tho-bre <tho.bremond+github@gmail.com>
2024-09-08 16:53:01 +0200

    Update package.json and tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false,
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'media', // ou 'class'
   theme: {
     extend: {},
   },
