95e051705a9b2fefdc4fbe26671a2df7b24de76a
tho-bre <tho.bremond+github@gmail.com>
2024-09-09 14:16:07 +0200

    Mettre à jour la couleur de fond pour l'iPhone 12 Pro / 13 Pro et le bandeau iOS

--- src/App.js
+++ src/App.js
@@ -8,11 +8,14 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [selectedCocktail, setSelectedCocktail] = useState(null);
 
-  const filteredCocktails = cocktails.filter(cocktail => 
-    cocktail.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-    cocktail.ingredients.some(ing => ing.name.toLowerCase().includes(searchTerm.toLowerCase()))
-  )
-  .sort((a, b) => a.name.localeCompare(b.name));
+  const filteredCocktails = cocktails.filter(cocktail => {
+    const searchTerms = searchTerm.toLowerCase().split(' ');
+    return searchTerms.every(term =>
+      cocktail.name.toLowerCase().includes(term) ||
+      cocktail.ingredients.some(ing => ing.name.toLowerCase().includes(term))
+    );
+  })
+  .sort((a, b) => a.name.localeCompare(b.name));  
 
   return (
     <div className="app-container">
@@ -24,13 +27,33 @@ const App = () => {
           <CocktailDetail cocktail={selectedCocktail} onBack={() => setSelectedCocktail(null)} />
         ) : (
           <>
-            <input
-              type="text"
-              placeholder="Rechercher un cocktail..."
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-              className="search-input"
-            />
+            <div className="search-container relative">
+              <input
+                  type="text"
+                  placeholder="Rechercher un cocktail..."
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                  className="search-input border border-gray-300 rounded-full py-2 px-4 w-full focus:outline-none focus:ring-2 focus:ring-red-600 focus:border-transparent"
+                />
+                {searchTerm && (
+                  <button
+                    onClick={() => setSearchTerm('')}
+                    style={{
+                      position: 'absolute',
+                      right: '12px',
+                      top: '50%',
+                      transform: 'translateY(-50%)',
+                      margin: 'auto',
+                      height: 'fit-content',
+                    }}
+                    className="text-gray-400 hover:text-red-600 focus:outline-none"
+                  >
+                    <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
+                      <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
+                    </svg>
+                  </button>
+              )}
+            </div>
             <CocktailList 
               cocktails={filteredCocktails} 
               onSelectCocktail={setSelectedCocktail} 
